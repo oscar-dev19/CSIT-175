c564b07eca1b0d88d29bb29c3518d5a7fec70582
Oscar Lopez <oscarl.developer@gmail.com>
2021-05-03 13:13:34 -0700

    Refactored race as its own function. Current Issue having suspicion that score is not being calculated correctly.

--- asgn9_Lopez_driver.py
+++ asgn9_Lopez_driver.py
@@ -2,17 +2,46 @@ from asgn9_Lopez_module import *
 from random import randrange
 
 
-def drive(car):
+def drive(car,currentscore):
     r = randrange(1,5)
-    d = car.showLine() * r
-    return f'{car.getColor()}:{d}>'
+    new_score = currentscore + r
+    d = car.showLine() * new_score
+    print(f'{car.getColor()}:{d}({new_score})>')
+    return new_score
 
 def main():
-    goal = 50
+    race(50)
+    
+
+    
+def race(goal):
+    print('Let the Race Begin!\n')
+    print(f'* * The first car to {goal} wins * *')
+    goal = goal
     playerScore = 0
     computerScore = 0
-    m1 = Model1()
-    print(drive(m1))
+    myCar = Model1()
+    print(f'Your car is a Model 1 and the color is {myCar.getColor()}')
+    computerCar = Model2()
+    print(f"The Computer's car is a Model 2 an the color is {computerCar.getColor()}")
+    while True:
+        winner = None
+        playerScore += drive(myCar,playerScore)
+        computerScore += drive(computerCar,computerScore)
+        if playerScore >= goal:
+            winner = 'You'
+            print(f'Winner is {winner}')
+            break
+        elif computerScore >= goal:
+            winner = 'Computer'
+            print(f'Winner is {winner}')
+            break
+        response = input("Drive some more? (y/n): ")
+        if response.lower() == 'n':
+            break
+        else:
+            continue
+    
     
     
 if __name__ == '__main__':
